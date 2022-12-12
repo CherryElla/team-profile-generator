78913fbffa95da52129ff7e8f8e4c6cebcaa5b8c
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-12-12 18:29:09 -0500

    editing class methods

--- index.js
+++ index.js
@@ -1,131 +1,135 @@
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+// const managerClass = require("./lib/manager")
+import Manager from './lib/manager'
 
-const fs = require("fs")
+const fs = require("fs");
 
-const start = () =>{
+let employees = {
+    manager: null,
+    interns: [],
+    engineers: []
+}
+
+const start = () => {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "Welcome, please enter your Team Manager's name: ",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's ID: ",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's email: ",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's office number: ",
-            name: "officeNo"
-        },
-        {
-            type: "list",
-            message: "Would you like to add another employe?",
-            choices: ["Add Intern", "Add Engineer", "Finish team building"],
-            name: "addEmployee"
-        },
-        
-    ])
-    .then((input) => {
-        if(input.addEmployee === "Add Intern") {
-            addIntern()
-    }
-    else if(input.addEmployee === "Add Engineer"){
-        addEngineer()
+        .prompt([
+            {
+                type: "input",
+                message: "Welcome, please enter your Team Manager's name: ",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's ID: ",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's email: ",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's office number: ",
+                name: "officeNo",
+            },
+            {
+                type: "list",
+                message: "Would you like to add another employe?",
+                choices: ["Add Intern", "Add Engineer", "Finish team building"],
+                name: "addEmployee",
+            },
+        ])
+        .then((input) => {
+            let manager = new Manager(input.name, input.id, input.email, input.officeNo)
+            employees.manager = manager
+            menuChoice(input.addEmployee)
+        });
+};
+
+const menuChoice = (choice) => {
+    console.log(choice)
+    if (choice === "Add Intern") {
+        addIntern();
+    } else if (choice === "Add Engineer") {
+        addEngineer();
     } else {
         generateHTML();
     }
-    });
-}
+};
 
 const addIntern = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Please enter your Intern's name: ",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's ID: ",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's email: ",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's school: ",
-            name: "school"
-        },
-        {
-            type: "list",
-            message: "Would you like to add another employe?",
-            choices: ["Add Engineer", "Finish team building"],
-            name: "addEmployee"
-        },
-    ])
-    .then((input) => {
-        if(input.addEmployee === "Add Enginner") {
-            addIntern()
-    }
-    else {
-        generateHTML()
-    }
-})
-}
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please enter your Intern's name: ",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's ID: ",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's email: ",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's school: ",
+                name: "school",
+            },
+            {
+                type: "list",
+                message: "Would you like to add another employe?",
+                choices: ["Add Intern", "Add Engineer", "Finish team building"],
+                name: "addEmployee",
+            },
+        ])
+        .then((input) => {
+            menuChoice(input)
+        });
+};
 
 const addEngineer = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Please enter your Engineer's name: ",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's ID: ",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's email: ",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Please enter employee's GitHub user: ",
-            name: "github"
-        },
-        {
-            type: "list",
-            message: "Would you like to add another employe?",
-            choices: ["Add Intern", "Finish team building"],
-            name: "addEmployee"
-        }, 
-    ])
-    .then((input) => {
-        if(input.addEmployee === "Add Intern") {
-            addIntern()
-    }
-    else {
-        generateHTML()
-    }
-})
-}
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please enter your Engineer's name: ",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's ID: ",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's email: ",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "Please enter employee's GitHub user: ",
+                name: "github",
+            },
+            {
+                type: "list",
+                message: "Would you like to add another employe?",
+                choices: ["Add Intern", "Add Engineer", "Finish team building"],
+                name: "addEmployee",
+            },
+        ])
+        .then((input) => {
+            menuChoice(input)
+        });
+};
 
 function generateHTML(fileName, data) {
-    fs.appendFile(file)
+    fs.appendFile(file);
 }
 
-
-start()
+start();
