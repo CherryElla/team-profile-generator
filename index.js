bcba49d89d2c4472fbe4bb2ae81aaba5857a7c65
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-12-12 13:01:49 -0500

    added inquirer function prompts

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ const inquirer = require("inquirer")
 
 const fs = require("fs")
 
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Team Manager's name: ",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Employee ID: ",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Employee email: ",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Employee office number: "
+        },
+        
+    ])
+    .then((input) => {
+        generateHTML("index.html", input)
+    })
 
 
-
-
+function generateHTML(fileName, data) {
+    fs.appendFile(file)
+}
 
 
