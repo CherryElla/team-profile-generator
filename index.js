5a68e9882c6d722767e0276927c38c88ad85b6d7
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-12-12 15:17:24 -0500

    built all prompts

--- index.js
+++ index.js
@@ -2,36 +2,130 @@ const inquirer = require("inquirer")
 
 const fs = require("fs")
 
-inquirer
+const start = () =>{
+    inquirer
     .prompt([
         {
             type: "input",
-            message: "Team Manager's name: ",
+            message: "Welcome, please enter your Team Manager's name: ",
             name: "name"
         },
         {
             type: "input",
-            message: "Employee ID: ",
+            message: "Please enter employee's ID: ",
             name: "id"
         },
         {
             type: "input",
-            message: "Employee email: ",
+            message: "Please enter employee's email: ",
             name: "email"
         },
         {
             type: "input",
-            message: "Employee office number: "
+            message: "Please enter employee's office number: ",
+            name: "officeNo"
+        },
+        {
+            type: "list",
+            message: "Would you like to add another employe?",
+            choices: ["Add Intern", "Add Engineer", "Finish team building"],
+            name: "addEmployee"
         },
         
     ])
     .then((input) => {
-        generateHTML("index.html", input)
-    })
+        if(input.addEmployee === "Add Intern") {
+            addIntern()
+    }
+    else if(input.addEmployee === "Add Engineer"){
+        addEngineer()
+    } else {
+        generateHTML();
+    }
+    });
+}
 
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter your Intern's name: ",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter employee's ID: ",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter employee's email: ",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter employee's school: ",
+            name: "school"
+        },
+        {
+            type: "list",
+            message: "Would you like to add another employe?",
+            choices: ["Add Engineer", "Finish team building"],
+            name: "addEmployee"
+        },
+    ])
+    .then((input) => {
+        if(input.addEmployee === "Add Enginner") {
+            addIntern()
+    }
+    else {
+        generateHTML()
+    }
+})
+}
+
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter your Engineer's name: ",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter employee's ID: ",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter employee's email: ",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter employee's GitHub user: ",
+            name: "github"
+        },
+        {
+            type: "list",
+            message: "Would you like to add another employe?",
+            choices: ["Add Intern", "Finish team building"],
+            name: "addEmployee"
+        }, 
+    ])
+    .then((input) => {
+        if(input.addEmployee === "Add Intern") {
+            addIntern()
+    }
+    else {
+        generateHTML()
+    }
+})
+}
 
 function generateHTML(fileName, data) {
     fs.appendFile(file)
 }
 
 
+start()
