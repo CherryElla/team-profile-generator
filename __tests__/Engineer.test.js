87c4ae65b6a7ca7a6e8621e3c1b5b0eb8bd7256c
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-12-18 15:14:21 -0500

    finished writing tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,38 @@
-describe("",() => {
+const Engineer = require("../lib/engineer")
+
+describe("Engineer",() => {
+    describe("Start Test", () => {
+        it("should initialize and have a valid GitHub", () => {
+            let name = "Tom";
+            let id = 2
+            let email = "dfdf@dfjj"
+            let role = "Engineer"
+            let github = "TomHarry"
+
+            let engineer = new Engineer(name, id, email, github)
+
+            expect(engineer.name).toEqual(name)
+            expect(engineer.id).toEqual(id)
+            expect(engineer.email).toEqual(email)
+            expect(engineer.getRole()).toEqual(role)
+            expect(engineer.getGithub()).toEqual(github)
+        })
+        // it("should give an error if no name was given", () => {
+        //     let callback = () => { 
+        //         new Engineer()
+        //     }
+        //     let error = new Error(
+        //         "Please enter a valid name."
+        //     ) 
+        //     expect(callback).toThrowError(error)
+        // })
+        it("should give an error if no GitHub value is given", () => {
+            let callback = () => {
+                new Engineer("Harry")
+            }
+            let error = new Error("Please enter a valid GitHub")
+            expect(callback).toThrowError(error)
+        })
+    })
     
 })
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,3 +1,29 @@
-describe("",() => {
+const Intern = require("../lib/intern")
+
+describe("Intern",() => {
+    describe("Start Test", () => {
+        it("should initialize and provide a valid school string", () => {
+            let name = "Tom";
+            let id = 2
+            let email = "dfdf@dfjj"
+            let role = "Intern"
+            let school = "Davidson School"
+
+            let intern = new Intern(name,id,email,school)
+
+            expect(intern.name).toEqual(name)
+            expect(intern.id).toEqual(id)
+            expect(intern.email).toEqual(email)
+            expect(intern.getRole()).toEqual(role)
+            expect(intern.getSchool()).toEqual(school)
+        })
+        it("give an error if value of school is given is not a string", () => {
+            let callback = () =>{
+                new Intern()
+            }
+            let error = new Error("Expected to be a string")
+            expect(callback).toThrowError(error)
+    })
     
+})
 })
